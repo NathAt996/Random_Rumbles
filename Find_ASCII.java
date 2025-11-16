87148d849715ea5eb9e0b470f1ec86eb8da31d97
NathAt996 <n.morgan5196@gmail.com>
2025-11-16 10:30:12 +0000

    Find_ASCII
    This program asks the user to input a character. The program then prints the corresponding ASCII code.

---
+++ Find_ASCII.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Find_ASCII {
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner (System.in);
+        
+        System.out.println("Get an ASCII from a character");
+        System.out.println("\nEnter a character : ");
+
+        String character = userInput.next();
+
+        int convertCharacter = character.charAt(0);
+
+        int userCharacter =  convertCharacter;
+
+        System.out.println("The ASCII code of " + character + " is : " + userCharacter);
+
+    }
+}
