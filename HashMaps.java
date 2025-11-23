79cba5886569bf4097ad582e1d25b6861fe3c5d1
NathAt996 <n.morgan5196@gmail.com>
2025-11-23 08:32:42 +0000

    HashMap
    This program displays a basic Hash Map.

---
+++ HashMaps.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+
+public class HashMaps {
+
+    public static void main(String[] args) {
+        
+        HashMap<String , Integer> map = new HashMap<>();
+
+
+        map.put("Alice" , 28);
+        map.put("Bob", 35);
+        map.put("David" , 23);
+
+
+
+
+        System.out.println("Original map : ");
+
+
+        for(String key : map.keySet()) {
+
+            System.out.println("Key : " + key + ", age " + map.get(key));
+        }
+
+
+        map.put("Bob", 36);
+
+
+        map.remove("David");
+
+
+        System.out.println("\nUpdated map : ");
+
+
+        for(String key : map.keySet()) {
+
+            System.out.println("Key : " + key + ", age " + map.get(key));
+        }
+    }
+}
