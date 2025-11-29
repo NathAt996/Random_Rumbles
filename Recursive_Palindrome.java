9657255103bdb033fd1eaffc5e88d9ba29360f13
NathAt996 <n.morgan5196@gmail.com>
2025-11-29 16:30:08 +0000

    Recursion on Palindrome
    Similar to my Palindrome detector program only this uses a recursive method to test each charAt before confirming true or false value.

---
+++ Recursive_Palindrome.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Recursive_Palindrome {
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner (System.in);
+
+        System.out.println("Enter a word. Lets test if a palindrome");
+        String word = userInput.nextLine();
+
+        System.out.println("Is " + firstLetterCapitalise(word) + " a palindrome? " + isPalindrome(word));
+
+    }
+
+    public static String firstLetterCapitalise (String str) {
+
+        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+    }
+
+    public static boolean isPalindrome (String str) {
+
+        if (str.length() <= 1) {
+            return true;
+        }
+
+        else if (str.charAt(0) != str.charAt(str.length() - 1)) {
+            return false;
+        }
+
+        else {
+            str.substring(1, str.length() - 1);
+            return true;
+        }
+    }
+}
