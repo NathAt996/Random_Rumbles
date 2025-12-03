d72f2346df70411a4312702055703902792784d9
NathAt996 <n.morgan5196@gmail.com>
2025-12-03 16:05:00 +0000

    Exception_Handling BASIC
    This is a simple program to practise my exception handling in Java.

---
+++ Exception_Handling.java
@@ -0,0 +1,32 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exception_Handling {
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        try {
+
+            System.out.println("Enter name");
+            String name = userInput.nextLine();
+
+            System.out.println("\nEnter age");
+            int age = userInput.nextInt();
+
+            System.out.println("\nHello " + name);
+            System.out.println("\nYou are " + age);
+
+        } catch (InputMismatchException e) {
+            System.out.println("You must input text for your name");
+
+        } catch (NumberFormatException e) {
+            System.out.println("You must enter an integer for your age");
+
+        } finally {
+            userInput.close();
+        }
+
+    }
+}
