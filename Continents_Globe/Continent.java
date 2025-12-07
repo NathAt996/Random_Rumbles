dbb217761d28ef548160342dd02762a0a1f03051
NathAt996 <n.morgan5196@gmail.com>
2025-12-07 16:08:14 +0000

    Continent_Interface
    This program uses an interface, getter methods and an array list to print interesting information about the 7 continents of the world. Additionally, it uses a boolean operator to confirm if the continent is a recognised continent.

---
+++ Continents_Globe/Continent.java
@@ -0,0 +1,55 @@
+package Continents_Globe;
+
+public class Continent implements Continent_Interface {
+
+    private String name;
+    private String populationSize;
+    private long numberOfLanguages;
+    private int numberOfCountries;
+    private boolean isContinent;
+
+    public Continent(String name, String populationSize, long numberOfLanguages, int numberOfCountries, boolean isContinent) {
+        this.name = name;
+        this.populationSize = populationSize;
+        this.numberOfLanguages = numberOfLanguages;
+        this.numberOfCountries = numberOfCountries;
+        this.isContinent = isContinent;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Name of continent: " + name);
+        System.out.println("Population size of " + name + ": " + populationSize);
+        System.out.println("Number of languages in " + name + ": " + numberOfLanguages);
+        System.out.println("Number of countries in " + name + ": " + numberOfCountries);
+    }
+
+    @Override
+    public String populationSize() {
+        return populationSize;
+    }
+
+    @Override
+    public long numberOfLanguages() {
+        return numberOfLanguages;
+    }
+
+    @Override
+    public int numberOfCountries() {
+        return numberOfCountries;
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public boolean isContinent() {
+        if(isContinent == false) {
+            System.out.println("is not recognised as a continent");
+        }
+
+        return false;
+    }
+}
---
+++ Continents_Globe/Continent_Interface.java
@@ -0,0 +1,11 @@
+package Continents_Globe;
+
+public interface Continent_Interface {
+
+    void displayDetails();
+    String populationSize();
+    long numberOfLanguages();
+    int numberOfCountries();
+    String name();
+    boolean isContinent();
+}
