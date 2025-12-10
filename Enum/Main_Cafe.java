2af01a6ef467e203902e02dc78806165e93c6a8d
NathAt996 <n.morgan5196@gmail.com>
2025-12-10 08:25:47 +0000

    Main Cafe updated
    Main Cafe updated to display increments of total cost given user my variety and dynamicism in choice

--- Enum/Main_Cafe.java
+++ Enum/Main_Cafe.java
@@ -8,53 +8,69 @@ public class Main_Cafe {
     public static void main(String[] args) {
 
         Scanner userInput = new Scanner(System.in);
-        Cafe order = null;
-        boolean validChoice = false;
+        Cafe order;
+        double totalCost = 0.00d;
+        boolean finishedOrder = false;
 
         System.out.println("Welcome to the coffee shop");
 
-        while (!validChoice) {
+        while (!finishedOrder) {
 
-            System.out.println("\nChoose your order: " +
-                    "\nSmall (press 1)" +
-                    "\nMedium (press 2)" +
-                    "\nLarge (press 3)");
+            boolean validChoice = false;
 
-            try {
-                int choice = userInput.nextInt();
 
-                if (choice == 1) {
-                    order = Cafe.SMALL;
-                    validChoice = true;
+            while (!validChoice) {
+
+                System.out.println("\nChoose your order: " +
+                        "\nSmall (press 1)" +
+                        "\nMedium (press 2)" +
+                        "\nLarge (press 3)" +
+                        "\nTo pay (press 0)");
+
+                try {
+
+                    int choice = userInput.nextInt();
+
+                    if (choice == 0) {
+                        finishedOrder = true;
+                        validChoice = true;
+                        continue;
+                    }
+
+                    if (choice == 1) {
+                        order = Cafe.SMALL;
+                        validChoice = true;
+                    }
+                    else if (choice == 2) {
+                        order = Cafe.MEDIUM;
+                        validChoice = true;
+                    }
+                    else if (choice == 3) {
+                        order = Cafe.LARGE;
+                        validChoice = true;
+                    }
+
+                    else {
+                        System.out.println("Input out of range. Please press 1, 2 or 3.");
+                        continue;
+                    }
+
+                    totalCost += order.getPrice();
+                    System.out.println(order.getSize() + " coffee added to your order");
+                    System.out.printf("Current total: £%.2f\n", totalCost);
+
+
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input type. Please press 1, 2 or 3 (using numbers).");
+                    userInput.next();
                 }
-                else if (choice == 2) {
-                    order = Cafe.MEDIUM;
-                    validChoice = true;
-                }
-                else if (choice == 3) {
-                    order = Cafe.LARGE;
-                    validChoice = true;
-                }
-
-                else {
-                    System.out.println("Input out of range. Please press 1, 2 or 3.");
-                }
-
-            }
-
-            catch (InputMismatchException e) {
-                System.out.println("Invalid input type. Please press 1, 2 or 3 (using numbers).");
-                userInput.next();
             }
         }
 
-        displayOrder(order);
-        userInput.close();
-    }
-
-    public static void displayOrder(Cafe coffee) {
-
-        System.out.println("\nOrder received");
-        System.out.println("For a " + coffee.getSize() + " coffee the price is £" + coffee.getPrice());
+        if (finishedOrder) {
+            System.out.printf("\nTotal Cost: £%.2f\n", totalCost);
+            System.out.println("\nHave a great day!");
+            userInput.close();
+        }
     }
 }
